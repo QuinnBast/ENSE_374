ed9420ba81a2b6a535f4ecfbb17a7d436fe26207
QuinnBast <Quinn.Bast@hotmail.com>
2016-10-15 17:27:31 -0600

    Added User Interface Class.

--- Lab3/src/LinkedListProgram.java
+++ Lab3/src/LinkedListProgram.java
@@ -6,24 +6,6 @@ public class LinkedListProgram {
 	public static void main(String args[])
 	{
 
-		LList.addElement(0);
-		LList.addElement(1);
-		LList.addElement(2);
-		LList.addElement(3);
-		LList.addElement(4);
-		
-		LList.getElement(0);	//Should find, OK
-		LList.getElement(1);	//Should find, OK
-		LList.getElement(4);	//Should find, OK
-		LList.getElement(7);	//Should not Find, OK
-		
-		LList.deleteElement(2);	//Should delete, OK
-		LList.getElement(4);	//Should not find after deleting, OK
-		
-		LList.deleteElement(4);	//Should not find, OK
-		
-		LList.printLinkedListTail();
-		LList.printLinkedListHead();
-	}
 
+}
 }
