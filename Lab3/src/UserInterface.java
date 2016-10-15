0f744145466ed6d22d7b091198df57c7bf35d000
QuinnBast <Quinn.Bast@hotmail.com>
2016-10-15 17:01:46 -0600

    Creating a UserInterface for a menu.

---
+++ Lab3/src/UserInterface.java
@@ -0,0 +1,13 @@
+
+public class UserInterface {
+	
+	public void displayMenu(){
+	System.out.println("---Menu---");
+	System.out.println("Choose an option:");
+	System.out.println("1. Add Node");
+	System.out.println("2. Search for Node at Index");
+	System.out.println("3. Delete Node at Index");
+	System.out.println("4. Print List");
+	}
+
+}
